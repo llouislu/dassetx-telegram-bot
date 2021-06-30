7c68f7652d1b3116d3ed44c4bfa0b34a151dc4fc
llouislu <louis.dev@outlook.com>
2021-06-30 21:13:04 +1200

    Added price provider

--- dassetx_telegram_bot/data_provider/dassetx_price_provider.py
+++ dassetx_telegram_bot/data_provider/dassetx_price_provider.py
@@ -1,12 +1,16 @@
+import logging
 import math
 from dataclasses import dataclass, field
 from typing import List, Dict
 
 import requests
+from telegram.ext import JobQueue
 
 from dassetx_telegram_bot.config import Config
 from dassetx_telegram_bot.data_provider.abc_provider import DataProviderABC
 from dassetx_telegram_bot.util.ddecorator import singleton
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -16,25 +20,42 @@ class DassetxPrice:
     askRate: float = field(default=math.inf)
     symbol: str = field(default=None)
 
+    @property
+    def price(self):
+        if self.lastTradeRate > 10:
+            return round(self.lastTradeRate, 3)
+        return self.lastTradeRate
+
 
 @singleton
 class DassetxPriceProvider(DataProviderABC):
-    def __init__(self):
+    def __init__(self, job_queue: JobQueue):
+        self._job_queue = job_queue
         self._session = requests.Session()
         self._tickers = {}
-        # TODO: make it subscribable i.e. on btc price updates,
-        #  run an external callback method to send alert messages
+        self.is_running = False
 
     def __del__(self):
+        self._job_queue.stop()
         self._session.close()
+
+    def start(self):
+        if not self.is_running:
+            self._job_queue.run_repeating(
+                self._get_tickers, interval=1.5, first=0.0
+            )
 
     def get(self, key: str) -> DassetxPrice:
         if key in self._tickers:
             return self._tickers.get(key)
         elif f"{key}-NZD" in self._tickers:
             return self._tickers.get(f"{key}-NZD")
+        else:
+            return None
 
-    def _get_tickers(self):
+    def _get_tickers(self, context):
+        if not self.is_running:
+            self.is_running = True
         # https://developers.dassetx.com/#428520eb-153b-461d-adb3-f51ca1216232
         r = self._session.get(
             "https://api.dassetx.com/api/markets/tickers",
@@ -42,11 +63,14 @@ class DassetxPriceProvider(DataProviderABC):
                 "x-account-id": Config.DASSETX_ACCOUNT_ID,
                 "x-api-key": Config.DASSETX_API_SECRET,
             },
+            timeout=3,
         )
         try:
+            logger.error(r.json())
             self._update_tickers(r.json())
         except Exception as e:
-            return
+            logger.exception(e)
+            pass
 
     def _update_tickers(self, tickers: List[Dict[str, str]]):
         # [
--- dassetx_telegram_bot/main.py
+++ dassetx_telegram_bot/main.py
@@ -1,30 +1,41 @@
-from telegram import ParseMode
-from telegram.ext import Updater, Defaults
+import logging
+
+from telegram.ext import Updater
 
 from dassetx_telegram_bot.bot.handler import (
     DassetxPriceAlertHandler,
 )
 from dassetx_telegram_bot.config import Config
+from dassetx_telegram_bot.data_provider.dassetx_price_provider import (
+    DassetxPriceProvider,
+)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def main():
-    updater = Updater(
-        token=Config.TELEGRAM_TOKEN,
-        defaults=Defaults(parse_mode=ParseMode.HTML),
-    )
+    updater = Updater(token=Config.TELEGRAM_TOKEN)
+
     dispatcher = updater.dispatcher
-    price_handler = DassetxPriceAlertHandler()
+    dassetxPriceProvider = DassetxPriceProvider(job_queue=updater.job_queue)
+
+    price_handler = DassetxPriceAlertHandler(
+        dassetxPriceProvider=dassetxPriceProvider
+    )
 
     dispatcher.add_handler(
-        'start', price_handler.start_command
+        price_handler.build_handler('start')
     )  # Accessed via /start
     dispatcher.add_handler(
-        'alert', price_handler.price_alert
+        price_handler.build_handler('alert')
     )  # Accessed via /alert
 
-    updater.start_polling()  # Start the bot
+    dassetxPriceProvider.start()
+    price_handler.start()
 
-    updater.idle()  # Wait for the script to be stopped, this will stop the bot as well
+    updater.start_polling()  # Start the bot
+    updater.idle()
 
 
 if __name__ == '__main__':
